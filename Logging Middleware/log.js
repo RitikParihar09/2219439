f0f3747356820ec8e60a7f841d4b9478d271f044
Ritik Parihar <146638282+RitikParihar09@users.noreply.github.com>
2025-07-15 11:29:59 +0530

    Update log.js

--- Logging Middleware/log.js
+++ Logging Middleware/log.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+
 async function Log(level, package, message) {
   try {
     const validLevels = ["debug", "info", "warn", "error", "fatal"];
@@ -9,7 +10,7 @@ async function Log(level, package, message) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJyaXRpa3BhcmloYXIuMjIwMTE1NTRAZ2VodS5hYy5pbiIsImV4cCI6MTc1MjU1NzgwNywiaWF0IjoxNzUyNTU2OTA3LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiNTUwYzA1ZTctYWMyMC00NjgzLWJhYmUtZjRlMGM0MmU0Y2NjIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoicml0aWsgcGFyaWhhciIsInN1YiI6IjQyYWEzY2M2LTU4MjAtNDllNi1hNjY2LTZhMjAwZjc0NTRjNiJ9LCJlbWFpbCI6InJpdGlrcGFyaWhhci4yMjAxMTU1NEBnZWh1LmFjLmluIiwibmFtZSI6InJpdGlrIHBhcmloYXIiLCJyb2xsTm8iOiIyMjE5NDM5IiwiYWNjZXNzQ29kZSI6IlFBaERVciIsImNsaWVudElEIjoiNDJhYTNjYzYtNTgyMC00OWU2LWE2NjYtNmEyMDBmNzQ1NGM2IiwiY2xpZW50U2VjcmV0IjoidFpQRkRoSmFKemdTd1hQcCJ9.UT9c4BKenbbjGYniN1dP4OwFAcLLzNqUXgIMh6xOrbI"
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJyaXRpa3BhcmloYXIuMjIwMTE1NTRAZ2VodS5hYy5pbiIsImV4cCI6MTc1MjU1OTU0NSwiaWF0IjoxNzUyNTU4NjQ1LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiNjI1MjAwYmEtOGFjNy00MDg5LThkN2ItMGU4MTVmYzNlZDM5IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoicml0aWsgcGFyaWhhciIsInN1YiI6IjQyYWEzY2M2LTU4MjAtNDllNi1hNjY2LTZhMjAwZjc0NTRjNiJ9LCJlbWFpbCI6InJpdGlrcGFyaWhhci4yMjAxMTU1NEBnZWh1LmFjLmluIiwibmFtZSI6InJpdGlrIHBhcmloYXIiLCJyb2xsTm8iOiIyMjE5NDM5IiwiYWNjZXNzQ29kZSI6IlFBaERVciIsImNsaWVudElEIjoiNDJhYTNjYzYtNTgyMC00OWU2LWE2NjYtNmEyMDBmNzQ1NGM2IiwiY2xpZW50U2VjcmV0IjoidFpQRkRoSmFKemdTd1hQcCJ9.A_XDyDSsCUVJnswE8cFOsbX0go-G0x52cTriQXXEAVQ"
       },
       body: JSON.stringify({
         stack: "frontend",
@@ -20,9 +21,11 @@ async function Log(level, package, message) {
     });
 
     const responseBody = await res.json();
-    console.log("✅ Log sent:", responseBody);
+    console.log("✅ Log sent:", responseBody.logID, "Message:", responseBody.message);
+    return responseBody.logID; 
   } catch (err) {
     console.error("❌ Log failed:", err.message);
   }
 }
-Log("info", "api", "User clicked login button");
+
+module.exports = { Log };
